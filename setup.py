fce90334b50b089fc80b74ed8e9aa6de31c56eea
Drew Verlee <drew.verlee@atomicobject.com>
2015-04-03 14:33:40 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="which_plates",
+    packages=["which_plates"],
+    version="1.0.0",
+    author="drew verlee",
+    author_email="drew.verlee@gmail.com",
+    url="https://github.com/drewverlee/which_plates"
+)
+
