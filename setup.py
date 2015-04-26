f4adfb0a5bfe5c3f4835fa1a72f07b3c01d710c6
Drew Verlee <drew.verlee@atomicobject.com>
2015-04-26 12:29:02 -0400

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="which_plates",
     packages=["which_plates"],
-    version="1.0.0",
+    version="1.0.1",
     author="drew verlee",
     author_email="drew.verlee@gmail.com",
     url="https://github.com/drewverlee/which_plates"
